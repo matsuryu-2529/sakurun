73eb4a4f8b9940a4c23f3643523cdf1fae50b2d3
Ryunosuke Matsuokaa <sirosakana014kenko@outlook.jp>
2024-05-19 21:37:22 +0900

    PDCAの編集画面表示

--- src/app/Livewire/PdcaTabBar.php
+++ src/app/Livewire/PdcaTabBar.php
@@ -8,6 +8,7 @@ class PdcaTabBar extends Component
 {
     public $activeTab = 'plan';
     public $activeSubject = '数学1';
+    public $edit = false;
 
     protected $listeners = ['activeSubjectUpdated'];
 
@@ -16,9 +17,25 @@ class PdcaTabBar extends Component
         $this->activeTab = $tab;
     }
 
+    public function setEdit()
+    {
+        $this->edit = true;
+    }
+
+    public function
+    resetEdit()
+    {
+        $this->edit = false;
+    }
+
     public function activeSubjectUpdated($subject)
     {
         $this->activeSubject = $subject;
+    }
+
+    public function aiReview()
+    {
+        
     }
 
     public function render()
