0d9a5047a1e6770709e8bbb3bee572924ddc6223
Ryunosuke Matsuokaa <sirosakana014kenko@outlook.jp>
2024-06-05 05:44:35 +0900

    ページネーション

--- src/resources/views/pagination.blade.php
+++ src/resources/views/pagination.blade.php
@@ -1,35 +1,33 @@
 @if ($paginator->hasPages())
     <nav>
-        <ul class="pagination">
+        <div class="pagination">
             @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>&lt;</span></li>
+                <button class="page" disabled>&lt;</button>
             @else
-                <li><button class="prev-page" wire:click="previousPage" wire:loading.attr="disabled" rel="prev">&lt;</button></li>
+                <button class="page" wire:click="previousPage" wire:loading.attr="disabled" rel="prev">&lt;</button>
             @endif
 
             @foreach ($elements as $element)
-                {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="disabled" aria-disabled="true"><span>{{ $element }}</span></li>
+                    <button class="page" disabled>{{ $element }}</button>
                 @endif
 
-                {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="active" aria-current="page"><span>{{ $page }}</span></li>
+                            <button class="page active">{{ $page }}</button>
                         @else
-                            <li><button wire:click="gotoPage({{ $page }})" class="page-link">{{ $page }}</button></li>
+                            <button class="page" wire:click="gotoPage({{ $page }})">{{ $page }}</button>
                         @endif
                     @endforeach
                 @endif
             @endforeach
 
             @if ($paginator->hasMorePages())
-                <li><button class="next-page" wire:click="nextPage" wire:loading.attr="disabled" rel="next">&gt;</button></li>
+                <button class="page" wire:click="nextPage" wire:loading.attr="disabled" rel="next">&gt;</button>
             @else
-                <li class="disabled" aria-disabled="true"><span>&gt;</span></li>
+                <button class="page" disabled>&gt;</button>
             @endif
-        </ul>
+        </div>
     </nav>
 @endif
